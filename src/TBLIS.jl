cc3c5cf5ef6da9fb579eb0ae65ad2c9675313997
MattDavis <matthew.davis010@gmail.com>
2020-06-03 14:37:41 -0400

    fixing tci missing error

--- src/TBLIS.jl
+++ src/TBLIS.jl
@@ -13,6 +13,9 @@ function init()
     if Sys.isapple() && !isfile("/usr/local/lib/libtci.0.dylib")
         loc = dirname(pathof(TBLIS))
         run(`ln -s $loc/libtci.0.dylib /usr/local/lib/libtci.0.dylib`)
+    elseif Sys.islinux() && !isfile("/usr/local/lib/libtci.so.0")
+        loc = dirname(pathof(TBLIS))
+        run(`ln -s $loc/libtci.so /usr/local/lib/libtci.so.0`)
     end
     global tblis = dlopen(joinpath(dirname(pathof(TBLIS)),"libtblis"))
 end
