76549955b8b7b15c57d05fb9d28a24673717c987
MattDavis <matthew.davis010@gmail.com>
2020-06-03 14:40:22 -0400

    working on tci bug

--- src/TBLIS.jl
+++ src/TBLIS.jl
@@ -15,7 +15,7 @@ function init()
         run(`ln -s $loc/libtci.0.dylib /usr/local/lib/libtci.0.dylib`)
     elseif Sys.islinux() && !isfile("/usr/local/lib/libtci.so.0")
         loc = dirname(pathof(TBLIS))
-        run(`ln -s $loc/libtci.so /usr/local/lib/libtci.so.0`)
+        run(`sudo ln -s $loc/libtci.so /usr/local/lib/libtci.so.0`)
     end
     global tblis = dlopen(joinpath(dirname(pathof(TBLIS)),"libtblis"))
 end
