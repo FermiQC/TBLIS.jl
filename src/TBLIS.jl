10b659a0da02eb9de9ee3e4ef1812432c6663297
MattDavis <matthew.davis010@gmail.com>
2020-06-04 12:02:03 -0400

    simplify init function

--- src/TBLIS.jl
+++ src/TBLIS.jl
@@ -11,13 +11,6 @@ global tblis = C_NULL
 global tci = C_NULL
 
 function init()
-    if Sys.isapple() && !isfile("/usr/local/lib/libtci.0.dylib")
-        loc = dirname(pathof(TBLIS))
-        run(`ln -s $loc/libtci.0.dylib /usr/local/lib/libtci.0.dylib`)
-    elseif Sys.islinux() && !isfile("/usr/local/lib/libtci.so.0")
-        loc = dirname(pathof(TBLIS))
-        run(`sudo ln -s $loc/libtci.so /usr/local/lib/libtci.so.0`)
-    end
     global tci = dlopen(joinpath(dirname(pathof(TBLIS)),"libtci"))
     global tblis = dlopen(joinpath(dirname(pathof(TBLIS)),"libtblis"))
 end
